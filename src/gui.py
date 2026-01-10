2e096ac71949f0285c7949efb09b16d4baefb85c
LaRueGT <gtlarue@gmail.com>
2026-01-09 22:39:14 -0500

    chargen p2 struggle

--- src/gui.py
+++ src/gui.py
@@ -147,12 +147,10 @@ class Gui:
                                                 pos=(0, 0, 0),
                                                 frameTexture='../assets/gridpaper_tex.png')
         self.chargen_screen_frame.guiItem.get_state_def(0).set_tex_scale(TextureStage.getDefault(), 1, 0.53)
-        # Mirroring alignment_frame position and size
         self.ability_adjustment_frame = DirectFrame(parent=self.chargen_screen_frame,
                                                     frameColor=(.25, .25, .25, 0),
                                                     frameSize=(0, 0.8, -0.6, .0),
                                                     pos=(-0.969, 0, 0.15))
-
         self.adj_heading = DirectLabel(parent=self.ability_adjustment_frame,
                                        text="Adjust Ability Scores",
                                        text_font=self.label_font,
@@ -160,30 +158,6 @@ class Gui:
                                        text_align=TextNode.ALeft,
                                        pos=(0, 0, 0.05),
                                        frameColor=(0, 0, 0, 0))
-        # We'll use a Sizer to keep the SpinBoxes and Labels aligned
-        self.adj_list_sizer = DirectBoxSizer(orientation=DGG.VERTICAL,
-                                             parent=self.ability_adjustment_frame,
-                                             pos=(0, 0, -0.1))
-        self.spinboxes = {}
-        abilities = ["Strength", "Intelligence", "Wisdom"]
-        for abl in abilities:
-            row = DirectBoxSizer(orientation=DGG.HORIZONTAL)
-            label = DirectLabel(text=f"{abl}: ",
-                                   text_font=self.label_font,
-                                   text_scale=0.06,
-                                   text_align=TextNode.ALeft,
-                                   frameColor=(0, 0, 0, 0))
-            # SpinBox from DirectGuiExtension
-            sb = DirectSpinBox(scale=0.06,
-                               range=(3, 18),
-                               value=10,
-                               text_font=self.label_font)
-
-            row.addItem(label)
-            row.addItem(sb)
-            self.adj_list_sizer.addItem(row)
-            self.spinboxes[abl] = sb
-
         self.button_frame = DirectBoxSizer(orientation=DGG.HORIZONTAL,
                                            parent=self.base_frame,
                                            frameColor=(0, 0, 0, 0),
